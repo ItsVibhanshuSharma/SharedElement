85c5221b7b814c79f371f1f0df58c0421f24edb3
ItsVibhanshuSharma <vibhanshusharma1811@gmail.com>
2023-10-20 23:35:11 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import ListScreen from './src/Screen/ListScreen';
+import DetailScreen from './src/Screen/DetailScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="List" component={ListScreen} />
+        <Stack.Screen name="DetailScreen" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
